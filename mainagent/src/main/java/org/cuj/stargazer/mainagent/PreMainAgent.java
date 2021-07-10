6df405a3bb2ac416a60c891ec3d0635270ecefd7
cujamin <chbnjamin@gmail.com>
2021-07-10 22:27:59 +0800

    mainagent

---
+++ mainagent/src/main/java/org/cuj/stargazer/mainagent/PreMainAgent.java
@@ -0,0 +1,21 @@
+package org.cuj.stargazer.mainagent;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+
+public class PreMainAgent {
+    public static void premain(String agentArgs, Instrumentation inst) {
+        System.out.println("agentArgs : " + agentArgs);
+        inst.addTransformer(new DefineTransformer(), true);
+    }
+
+    static class DefineTransformer implements ClassFileTransformer {
+
+        @Override
+        public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) {
+            System.out.println("premain load Class:" + className);
+            return classfileBuffer;
+        }
+    }
+}
---
+++ test/src/main/java/org/cuj/stargazer/test/TestMain.java
@@ -0,0 +1,7 @@
+package org.cuj.stargazer.test;
+
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("Hello Java Agent");
+    }
+}
